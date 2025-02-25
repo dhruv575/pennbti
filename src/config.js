e1060d9ae74eb364bdb41ba7dbf2b31fa601d723
dhruv575 <dhruv575@gmail.com>
2025-02-25 13:44:26 -0500

    Fixed backend link for deployment

--- src/config.js
+++ src/config.js
@@ -1,2 +1,2 @@
-// export const API_URL = 'https://pennbti-backend-a39f64aea984.herokuapp.com'; 
-export const API_URL = 'http://localhost:5000'; 
+export const API_URL = 'https://pennbti-backend-a39f64aea984.herokuapp.com'; 
+// export const API_URL = 'http://localhost:5000'; 
